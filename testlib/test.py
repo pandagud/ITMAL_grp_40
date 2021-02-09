919c414590e84709c3d31355d2bf66ac836e4eb6
pandagud <pandagud@gmail.com>
2021-02-09 13:09:34 +0100

    first commit

---
+++ testlib/test.py
@@ -0,0 +1,14 @@
+import numpy as np
+import contextlib as ctxlib
+import collections
+import sklearn
+import random
+
+
+def TestName():
+    #NOTE: Returing test string name
+    return "Random_Name2"
+
+def RandomNumber():
+    #NOTE: Returing random number between 0 and 10
+    return random.randrange(10)
